1875bbe49f0f6a548d6698cd038ce0ae2de7aadc
Skrebkov Artem <artem@ubuntu.(none)>
2014-03-20 12:08:49 -0700

    Add glow implementation

--- photoeffects/include/photoeffects.hpp
+++ photoeffects/include/photoeffects.hpp
@@ -5,3 +5,5 @@
 cv::Mat sepia(cv::Mat& image);
 
 cv::Mat tint(cv::Mat& image, int hue, float sat);
+
+int glow(cv::Mat& image);
--- photoeffects/src/sepia.cpp
+++ photoeffects/src/sepia.cpp
@@ -6,3 +6,8 @@ Mat sepia(Mat& image)
 {
     return image;
 }
+
+int glow(cv::Mat& image)
+{
+	return image.cols;
+}
