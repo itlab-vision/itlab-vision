0099cc60b6672095cbd2c41f9be3cbe2a312a80b
Dolotov Evgeniy <dolotov.evgeniy@gmail.com>
2014-03-20 21:23:21 +0400

    Empty emplementation of the Film Grain filter (src/filmGrain.cpp) and fake test (test/filmGrain_test.cpp)

--- photoeffects/src/filmGrain.cpp
+++ photoeffects/src/filmGrain.cpp
@@ -2,10 +2,6 @@
 
 using namespace cv;
 
-int sepia(Mat& image)
-{
-    return image.cols;
-}
 int filmGrain(Mat& image,Mat& result)
 {
     return image.rows;
--- photoeffects/test/filmGrain_test.cpp
+++ photoeffects/test/filmGrain_test.cpp
@@ -4,15 +4,6 @@
 
 using namespace cv;
 
-TEST(photoeffects, PassingTest) {
-    EXPECT_TRUE(true);
-}
-
-TEST(photoeffects, SepiaTest) {
-    Mat image(10, 10, CV_8UC1);
-
-    EXPECT_EQ(10, sepia(image));
-}
 TEST(photoeffects, FilmGrainTest) {
     Mat image(10, 10, CV_8UC1);
 
