b5cdf571de24b7f8046268c3edc507bcac841f27
Skrebkov Artem <artem@ubuntu.(none)>
2014-03-27 15:24:13 -0700

    Test implementation for glow filter

--- photoeffects/samples/photoeffects_sample.cpp
+++ photoeffects/samples/photoeffects_sample.cpp
@@ -1,9 +1,18 @@
 #include "photoeffects.hpp"
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <fstream>
+
+using namespace cv;
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hey there!" << std::endl;
-
+    Mat image;
+    Mat dst;
+    image = imread("image-opacity.jpg", 1);
+    imwrite( "img.jpg", image );
+    Mat data(image);
+    image.convertTo(data, CV_32F);
+    glow(data, 9, 0.9, dst);
     return 0;
 }
