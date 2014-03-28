cc0f995af0a17779ccbda7cb620cf2fca4b1e9c0
Dolotov Evgeniy <dolotov.evgeniy@gmail.com>
2014-03-28 23:38:36 +0400

    add changes based on comments

--- photoeffects/samples/photoeffects_sample.cpp
+++ photoeffects/samples/photoeffects_sample.cpp
@@ -2,44 +2,61 @@
 #include <iostream>
 using namespace cv;
 using namespace std;
+
+const string ORIGINAL_IMAGE="Original image";
+const string FADED_IMAGE="Faded image";
+
+Point startPoint,endPoint;
+int numberChoosenPoint=0;
+
 void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 {
-     Point* p=(Point*)userdata;
-     if  ( event == EVENT_LBUTTONDOWN )
-     {
-          cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
-          p->x=x;
-          p->y=y;
-     }
+    Mat* image=(Mat*)userdata;
+    if  ( event == EVENT_LBUTTONDOWN )
+    {
+        numberChoosenPoint++;
+        switch(numberChoosenPoint)
+        {
+        case 1:
+            startPoint=Point(x,y);
+            break;
+        case 2:
+            endPoint=Point(x,y);
+            break;
+        }
+        circle(*image,Point(x,y),5,CV_RGB(255,50,255),4);
+        cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+        imshow(ORIGINAL_IMAGE,*image);
+
+    }
 }
 
 int main(int argc, char** argv)
 {
-    cout<< "Fade color function test:"<<endl;
-    cout<<"    Input name of image:";
-    string name;
-    cin>>name;
-    Mat src=imread(name, CV_LOAD_IMAGE_GRAYSCALE);
-    Mat dst(src.rows,src.cols,CV_8UC1);
+    Mat src=imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    if(!src.data)
+    {
+        cout<<"Not found file"<<endl;
+        return 0;
+    }
+    Mat dst(src.rows,src.cols,CV_8UC3);
     Mat srcCopy;
     src.copyTo(srcCopy);
-    namedWindow("original",CV_WINDOW_AUTOSIZE);
-    Point tmpPoint,startPoint,endPoint;
-    setMouseCallback("original", CallBackFunc,&tmpPoint);
-    imshow("original",srcCopy);
-    cvWaitKey(0);
-    startPoint=tmpPoint;
-    circle(srcCopy,startPoint,5,CV_RGB(255,255,255),4);
-    imshow("original",srcCopy);
-    cvWaitKey(0);
-    endPoint=tmpPoint;
-    line(srcCopy,startPoint, endPoint, CV_RGB(255,255,255),4,CV_AA);
-    imshow("original",srcCopy);
-    cvWaitKey(0);
+    namedWindow(ORIGINAL_IMAGE,CV_WINDOW_AUTOSIZE);
+    imshow(ORIGINAL_IMAGE,srcCopy);
+    setMouseCallback(ORIGINAL_IMAGE, CallBackFunc,&srcCopy);
+    do
+    {
+        cout<<"Choose two point and press any key"<<endl;
+        waitKey(0);
+    }
+    while(numberChoosenPoint!=2);
     fadeColor(src,dst,startPoint,endPoint);
-    namedWindow("Fade image",CV_WINDOW_AUTOSIZE);
-    imshow("Fade image",dst);
-    imwrite( "Fade_Image.jpg", dst );
-    cvWaitKey(0);
+    namedWindow(FADED_IMAGE,CV_WINDOW_AUTOSIZE);
+    imshow(FADED_IMAGE,dst);
+    cout << "Press any key to EXIT"<<endl;
+    waitKey(0);
+    imwrite( argv[2], dst );
+    destroyAllWindows();
     return 0;
 }
--- photoeffects/src/fadeColor.cpp
+++ photoeffects/src/fadeColor.cpp
@@ -1,17 +1,41 @@
 #include "photoeffects.hpp"
 #include <math.h>
+#include <vector>
 using namespace cv;
+using namespace std;
+Point findFarthestPoint(Point vector,Mat& image)
+{
+    if(vector.x<=0 && vector.y<=0)
+    {
+        return Point(0,0);
+    }
 
-int fadeColor(InputArray src, OutputArray dst,cv::Point startPoint,cv::Point endPoint)
+    if(vector.x<=0 && vector.y>0)
+    {
+        return Point(image.rows,0);
+    }
+
+    if(vector.x>0 && vector.y<=0)
+    {
+        return Point(0,image.cols);
+    }
+
+    if(vector.x>0 && vector.y>0)
+    {
+        return Point(image.rows,image.cols);
+    }
+}
+
+int fadeColor(InputArray src, OutputArray dst,Point startPoint,Point endPoint)
 {
     Mat image=src.getMat();
-    Point vector,newPoint ;
+    Point perpendicular,newPoint ;
     // perpendicular to the line
-    vector.x=-(startPoint.x-endPoint.x);
-    vector.y=-(startPoint.y-endPoint.y);
+    perpendicular.x=-(startPoint.x-endPoint.x);
+    perpendicular.y=-(startPoint.y-endPoint.y);
     //second point of the line
-    newPoint.x=startPoint.x+vector.y;
-    newPoint.y=startPoint.y-vector.x;
+    newPoint.x=startPoint.x+perpendicular.y;
+    newPoint.y=startPoint.y-perpendicular.x;
     //line equation: A*x+By+C=0
 
     int A=(startPoint.x-newPoint.x);
@@ -19,44 +43,26 @@ int fadeColor(InputArray src, OutputArray dst,cv::Point startPoint,cv::Point end
     int C=-startPoint.y*A-B*startPoint.x;
 
     //find the most distant point from the line
-    Point farthestPoint;
-    if(vector.x<=0 && vector.y<=0)
-    {
-        farthestPoint=Point(0,0);
-    }
-    else
-    {
-        if(vector.x<=0 && vector.y>0)
-        {
-            farthestPoint=Point(image.rows,0);
-        }
-        else
-        {
-            if(vector.x>0 && vector.y<=0)
-            {
-                farthestPoint=Point(0,image.cols);
-            }
-            else
-            {
-                if(vector.x>0 && vector.y>0)
-                {
-                    farthestPoint=Point(image.rows,image.cols);
-                }
-            }
-        }
-    }
+    Point farthestPoint=findFarthestPoint(perpendicular,image);
 
-    double maximumDistance=abs(A*farthestPoint.x+B*farthestPoint.y+C);
+
+    int maximumDistance=abs(A*farthestPoint.x+B*farthestPoint.y+C);
+    vector<Mat> planes;
+    split(image,planes);
     for(int i=0;i<image.rows;i++)
         for(int j=0;j<image.cols;j++)
         {
             double distance=abs(A*i+B*j+C);
             //change pixels only in the direction of the perpendicular
-            if((vector.x*(j-startPoint.x)+vector.y*(i-startPoint.y))>=0)
+            if((perpendicular.x*(j-startPoint.x)+perpendicular.y*(i-startPoint.y))>=0)
             {
-                 image.at<uchar>(i,j)=(image.at<uchar>(i,j)*(maximumDistance-distance)+255*distance)/maximumDistance;
+                for(int n=0;n<image.channels();n++)
+                {
+                 planes[n].at<uchar>(i,j)=(planes[n].at<uchar>(i,j)*(maximumDistance-distance)+255*distance)/maximumDistance;
+                }
             }
         }
-    image.copyTo(dst);
+
+    merge(planes,dst);
     return 0;
 }
