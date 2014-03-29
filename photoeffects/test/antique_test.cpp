276eb1bb1b40f0b9027094989e9b98cee11baf20
Vadim Levin <vadimlevin1@gmail.com>
2014-03-29 13:32:22 +0400

    Change antique_test.cpp

--- photoeffects/test/antique_test.cpp
+++ photoeffects/test/antique_test.cpp
@@ -6,7 +6,8 @@ using namespace cv;
 
 TEST(photoeffects, AntiqueTest) 
 {
-    Mat image(10, 10, CV_8UC1);
-    
-    EXPECT_EQ(10, antique(image).cols);
+    Mat src(10, 10, CV_8UC1);
+    Mat texture(10, 10, CV_8UC1);
+    Mat dst;
+    EXPECT_EQ(0, antique(src,texture,dst));
 }
