79c113a5ae7c48aff35a30bf0f7cfe790a82a2f7
Vadim Levin <vadimlevin1@gmail.com>
2014-03-29 15:33:39 +0400

    Trying use CMake

--- photoeffects/src/antique.cpp
+++ photoeffects/src/antique.cpp
@@ -4,5 +4,8 @@ using namespace cv;
 
 int antique(InputArray src, InputArray texture, OutputArray dst)
 {
+    resize(texture, texture, src.size());
+    merge(src, dst);
+    merge(texture, dst);  
     return 0;
 }
