cdeede838844a57671be1120318ec663b0129f5d
valentina-kustikova <valentina.kustikova@gmail.com>
2014-03-31 11:33:18 -0700

    Tests for sepia filter were written.

--- photoeffects/test/sepia_test.cpp
+++ photoeffects/test/sepia_test.cpp
@@ -4,8 +4,26 @@
 
 using namespace cv;
 
-TEST(photoeffects, SepiaTest) {
+TEST(photoeffects, SepiaFakeTest) {
     Mat src(10, 10, CV_8UC1), dst;
 
     EXPECT_EQ(0, sepia(src, dst));
+}
+
+TEST(photoeffects, SepiaFailTest) {
+    Mat src(10, 10, CV_8UC3), dst;
+
+    EXPECT_EQ(1, sepia(src, dst));
+}
+
+TEST(photoeffects, SepiaTest) {
+    Mat src(10, 10, CV_8UC1), dst, hsvDst;
+    vector<Mat> channels(3);
+
+    EXPECT_EQ(0, sepia(src, dst));
+    cvtColor(dst, hsvDst, CV_BGR2HSV);
+    split(hsvDst, channels);
+    EXPECT_EQ(20, channels[0].at<uchar>(0, 0));
+    EXPECT_EQ(77, channels[1].at<uchar>(0, 0));
+    EXPECT_EQ(src.at<uchar>(0, 0) + 20, channels[2].at<uchar>(0, 0));
 }
