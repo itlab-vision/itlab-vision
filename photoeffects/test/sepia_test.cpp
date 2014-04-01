a32291bc819f19126715b8cc7cc577fc5dc3a8f2
valentina-kustikova <valentina.kustikova@gmail.com>
2014-03-31 21:10:54 -0700

    Test was restored, interval [<v>-1,<v>+1] was set for hue, saturation and value.

--- photoeffects/test/sepia_test.cpp
+++ photoeffects/test/sepia_test.cpp
@@ -16,7 +16,6 @@ TEST(photoeffects, SepiaFailTest) {
     EXPECT_EQ(1, sepia(src, dst));
 }
 
-/*
 TEST(photoeffects, SepiaTest) {
     Mat src(10, 10, CV_8UC1), dst, hsvDst;
     vector<Mat> channels(3);
@@ -24,6 +23,11 @@ TEST(photoeffects, SepiaTest) {
     EXPECT_EQ(0, sepia(src, dst));
     cvtColor(dst, hsvDst, CV_BGR2HSV);
     split(hsvDst, channels);
-    EXPECT_EQ(src.at<uchar>(0, 0) + 20, channels[2].at<uchar>(0, 0));
+    EXPECT_LE(19 - 1, channels[0].at<uchar>(0, 0)); // hue = 19
+    EXPECT_GE(19 + 1, channels[0].at<uchar>(0, 0));
+    EXPECT_LE(78 - 1, channels[1].at<uchar>(0, 0)); // saturation = 78
+    EXPECT_GE(78 + 1, channels[1].at<uchar>(0, 0));
+    EXPECT_LE(src.at<uchar>(0, 0) + 20 - 1, channels[2].at<uchar>(0, 0));
+    EXPECT_GE(src.at<uchar>(0, 0) + 20 + 1, channels[2].at<uchar>(0, 0));
 }
-*/
+
