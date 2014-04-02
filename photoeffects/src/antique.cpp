d231ceba9517278521e5dab0a986f799129bd631
Vadim Levin <vadimlevin1@gmail.com>
2014-04-02 21:49:12 +0400

    Correction format

--- photoeffects/src/antique.cpp
+++ photoeffects/src/antique.cpp
@@ -6,13 +6,13 @@ int antique(InputArray src, InputArray texture, OutputArray dst)
 {
     Mat textureImg = texture.getMat();
     resize(texture,textureImg, src.size(),0,0, INTER_LINEAR);
-   	Mat m_sepiaKernel = (Mat_<float>(3,3)<<0.272f, 0.534f, 0.131f,  
-                                            0.349f, 0.686f, 0.168f, 
-                                            0.393f, 0.769f, 0.189f);
+    Mat m_sepiaKernel = (Mat_<float>(3,3)<<0.272f, 0.534f, 0.131f,
+                                           0.349f, 0.686f, 0.168f,
+                                           0.393f, 0.769f, 0.189f);
     Mat newSrc = src.getMat();
     transform(newSrc,newSrc,m_sepiaKernel);
     float alpha = 0.7f;
     float beta = 1.0f-alpha;
-    addWeighted(textureImg, alpha , newSrc, beta, 0.0, dst);
+    addWeighted(textureImg, alpha , newSrc, beta, 0.0f, dst);
     return 0;
 }
