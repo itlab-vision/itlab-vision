5bb7b03c5b1710be2e68b0a72a1ff05513546824
Dolotov Evgeniy <dolotov.evgeniy@gmail.com>
2014-04-03 21:53:49 +0400

    attempt to improve

--- photoeffects/samples/fadeColor_sample.cpp
+++ photoeffects/samples/fadeColor_sample.cpp
@@ -6,9 +6,38 @@ using namespace std;
 const string ORIGINAL_IMAGE="Original image";
 const string FADED_IMAGE="Faded image";
 
+const char *helper =
+"./fadeColor <img>\n\
+\t<img> - file name contained the processed image\n\
+";
+
 Point startPoint,endPoint;
 int numberChoosenPoint=0;
 
+void CallBackFunc(int event, int x, int y, int flags, void* userdata);
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        cout << helper << endl;
+        return 1;
+    }
+    Mat src=imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    if(!src.data)
+    {
+        cout<<"Not found file"<<endl;
+        return 1;
+    }
+    namedWindow(ORIGINAL_IMAGE,CV_WINDOW_AUTOSIZE);
+    imshow(ORIGINAL_IMAGE,src);
+    setMouseCallback(ORIGINAL_IMAGE, CallBackFunc,&src);
+    cout << "Choose two point on image"<<endl;
+    cout << "Press any key to EXIT"<<endl;
+    waitKey(0);
+    destroyAllWindows();
+    return 0;
+}
 void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 {
     Mat src=*((Mat*)userdata);
@@ -29,27 +58,12 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata)
             numberChoosenPoint++;
             Mat dst;
             fadeColor(src,dst,startPoint,endPoint);
+             circle(srcCopy,startPoint,5,CV_RGB(255,50,255),4);
+            circle(srcCopy,Point(x,y),5,CV_RGB(255,50,255),4);
+            imshow(ORIGINAL_IMAGE,srcCopy);
             namedWindow(FADED_IMAGE,CV_WINDOW_AUTOSIZE);
             imshow(FADED_IMAGE,dst);
             break;
         }
     }
-}
-
-int main(int argc, char** argv)
-{
-    Mat src=imread(argv[1], CV_LOAD_IMAGE_COLOR);
-    if(!src.data)
-    {
-        cout<<"Not found file"<<endl;
-        return 0;
-    }
-    namedWindow(ORIGINAL_IMAGE,CV_WINDOW_AUTOSIZE);
-    imshow(ORIGINAL_IMAGE,src);
-    setMouseCallback(ORIGINAL_IMAGE, CallBackFunc,&src);
-    cout << "Choose two picture on image"<<endl;
-    cout << "Press any key to EXIT"<<endl;
-    waitKey(0);
-    destroyAllWindows();
-    return 0;
 }
--- photoeffects/src/fadeColor.cpp
+++ photoeffects/src/fadeColor.cpp
@@ -1,46 +1,54 @@
 #include "photoeffects.hpp"
 #include <math.h>
 #include <vector>
-using namespace cv;
-using namespace std;
-
-#define sign(x) (( x > 0 ) - ( x < 0 ))
 
 using namespace cv;
 using namespace std;
-Point findFarthestPoint(Point vector,Mat& image)
+
+double scalarProduct(Point a, Point b)
+{
+    return a.x*b.x+a.y*b.y;
+}
+
+Point findFarthestPoint(Point vector, Mat& image)
 {
     Point farthestPoint;
     if(vector.x<=0 && vector.y<=0)
     {
-        farthestPoint=Point(0,0);
+        farthestPoint=Point(0, 0);
     }
 
     if(vector.x<=0 && vector.y>0)
     {
-        farthestPoint=Point(image.rows,0);
+        farthestPoint=Point(image.rows, 0);
     }
 
     if(vector.x>0 && vector.y<=0)
     {
-        farthestPoint=Point(0,image.cols);
+        farthestPoint=Point(0, image.cols);
     }
 
     if(vector.x>0 && vector.y>0)
     {
-        farthestPoint=Point(image.rows,image.cols);
+        farthestPoint=Point(image.rows, image.cols);
     }
+
     return farthestPoint;
 }
 
-int fadeColor(InputArray src, OutputArray dst,Point startPoint,Point endPoint)
+int fadeColor(InputArray src, OutputArray dst,
+              Point startPoint, Point endPoint)
 {
+    if (src.type() != CV_8UC1 && src.type() != CV_8UC3)
+    {
+        return 1;
+    }
     Mat image=src.getMat();
     Point perpendicular;
     // perpendicular to the line
 
-    perpendicular.x=-(startPoint.x-endPoint.x);
-    perpendicular.y=-(startPoint.y-endPoint.y);
+    perpendicular.x=endPoint.x-startPoint.x;
+    perpendicular.y=endPoint.y-startPoint.y;
     //line equation: A*x+By+C=0
 
     int A=-perpendicular.y;
@@ -48,10 +56,10 @@ int fadeColor(InputArray src, OutputArray dst,Point startPoint,Point endPoint)
     int C=-startPoint.y*A-B*startPoint.x;
 
     //find the most distant point from the line
-    Point farthestPoint=findFarthestPoint(perpendicular,image);
+    Point farthestPoint=findFarthestPoint(perpendicular, image);
 
 
-    int maximumDistance=abs(A*farthestPoint.x+B*farthestPoint.y+C);
+    int maxDistance=abs(A*farthestPoint.x+B*farthestPoint.y+C);
     vector<Mat> planes;
     split(image,planes);
     for(int i=0;i<image.rows;i++)
@@ -59,15 +67,20 @@ int fadeColor(InputArray src, OutputArray dst,Point startPoint,Point endPoint)
         {
             int distance=abs(A*i+B*j+C);
             //change pixels only in the direction of the perpendicular
-            if((perpendicular.x*(j-startPoint.x)+perpendicular.y*(i-startPoint.y))>=0)
+            Point directionVector(j-startPoint.x, i-startPoint.y);
+            if(scalarProduct(perpendicular, directionVector)>=0)
             {
                 for(int n=0;n<image.channels();n++)
                 {
-                 planes[n].at<uchar>(i,j)=(planes[n].at<uchar>(i,j)*(maximumDistance-distance)+255*distance)/maximumDistance;
+                    int channelValue=planes[n].at<uchar>(i,j);
+                    channelValue*=(maxDistance-distance);
+                    channelValue+=255*distance;
+                    channelValue/=maxDistance;
+                    planes[n].at<uchar>(i,j)=channelValue;
                 }
             }
         }
 
-    merge(planes,dst);
+    merge(planes, dst);
     return 0;
 }
