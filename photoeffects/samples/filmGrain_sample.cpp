d12e61c70d51879f47af5a42c5c636a395bf068c
Dolotov Evgeniy <dolotov.evgeniy@gmail.com>
2014-04-04 22:18:54 +0400

    add film grain implementation and sample

--- photoeffects/samples/filmGrain_sample.cpp
+++ photoeffects/samples/filmGrain_sample.cpp
@@ -4,13 +4,34 @@
 #include <opencv2/opencv.hpp>
 #include <math.h>
 using namespace cv;
+using namespace std;
+
+const char *ORIGINAL_IMAGE="Original image";
+const char *FILM_GRAIN_IMAGE="Faded image";
+const char *helper =
+"./filmGrain_sample <img>\n\
+\t<img> - file name contained the processed image\n\
+";
+
 int main(int argc, char** argv)
 {
-    Mat src=imread(argv[1], CV_LOAD_IMAGE_COLOR),dst;
-    namedWindow("ORIGINAL_IMAGE",CV_WINDOW_AUTOSIZE);
-    imshow("ORIGINAL_IMAGE",src);
+    if (argc < 2)
+    {
+        cout << helper << endl;
+        return 1;
+    }
+    Mat src=imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    if(!src.data)
+    {
+        cout<<"Not found file"<<endl;
+        return 1;
+    }
+    namedWindow(ORIGINAL_IMAGE,CV_WINDOW_AUTOSIZE);
+    imshow(ORIGINAL_IMAGE,src);
+    Mat dst;
     filmGrain(src,dst);
-    imshow("ORIGINAL_IMAGE",dst);
+    imshow(FILM_GRAIN_IMAGE,dst);
+    cout << "Press any key to EXIT"<<endl;
     waitKey(0);
     return 0;
 }
--- photoeffects/src/filmGrain.cpp
+++ photoeffects/src/filmGrain.cpp
@@ -2,31 +2,54 @@
 #include <stdlib.h>
 #include <opencv2/opencv.hpp>
 using namespace cv;
-#define MAX_VALUE 50
+#define MAX_VALUE 20
 int filmGrain(cv::InputArray src, cv::OutputArray dst)
 {
+    if (src.type() != CV_8UC1 && src.type() != CV_8UC3)
+    {
+        return 1;
+    }
     Mat imageRGB=src.getMat();
     Mat imageYUV;
     cvtColor(imageRGB,imageYUV,CV_RGB2YUV);
     vector<Mat> planes;
     split(imageYUV,planes);
-    for(int i=0;i<imageYUV.rows;i++)
-        for(int j=0;j<imageYUV.cols;j++)
-        {
-            int k=(planes[0].at<uchar>(i,j)+(rand()%MAX_VALUE-rand()%MAX_VALUE));
-            if(k<0)
+    if(src.type()==CV_8UC1)
+    {
+        for(int i=0;i<imageYUV.rows;i++)
+            for(int j=0;j<imageYUV.cols;j++)
             {
-                k=0;
+                int k=(imageYUV.at<uchar>(i,j)+(rand()%MAX_VALUE-rand()%MAX_VALUE));
+                if(k<0)
+                {
+                    k=0;
+                }
+                if(k>=256)
+                {
+                    k=255;
+                }
+                imageYUV.at<uchar>(i,j)= k;
             }
-            if(k>=256)
+    }
+    if(src.type()==CV_8UC3)
+    {
+        for(int i=0;i<imageYUV.rows;i++)
+            for(int j=0;j<imageYUV.cols;j++)
             {
-                k=255;
+                int k=(imageYUV.at<Vec3b>(i,j)[0]+(rand()%MAX_VALUE-rand()%MAX_VALUE));
+                if(k<0)
+                {
+                    k=0;
+                }
+                if(k>=256)
+                {
+                    k=255;
+                }
+                imageYUV.at<Vec3b>(i,j)[0]= k;
             }
-            planes[0].at<uchar>(i,j)= k;
-        }
-
+    }
     Mat dstYUV;
-    merge(planes,dstYUV);
+    imageYUV.copyTo(dstYUV);
     cvtColor(dstYUV,dst,CV_YUV2RGB);
     return 0;
 }
