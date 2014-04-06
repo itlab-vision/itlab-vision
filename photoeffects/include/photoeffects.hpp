fa9b324450aeed1c379001fcb252f10a3603557b
Vlad <vlad@vlad-Q330.(none)>
2014-04-07 00:51:15 +0400

    Changed photoeffects.hpp and vignette.cpp

--- photoeffects/include/photoeffects.hpp
+++ photoeffects/include/photoeffects.hpp
@@ -19,3 +19,5 @@ int boostColor(cv::InputArray src, cv::OutputArray dst, float intensity = 0.0f);
 cv::Mat antique(cv::Mat& image);
 
 int vignette(cv::InputArray src, cv::OutputArray dst, cv::Size rect);
+
+int warmify(cv::InputArray src, cv::OutputArray dst, uchar delta = 30);
