798c477509192bb803bf5fd2c6a3ef3157302984
KruchDmitriy <kruch.dmitriy@gmail.com>
2014-04-06 18:25:00 +0400

    Forget to add file

---
+++ photoeffects/samples/edgeBlur_sample.cpp
@@ -0,0 +1,48 @@
+#include "photoeffects.hpp"
+#include <stdio.h>
+
+using namespace cv;
+
+Mat img, filterImg;
+int indentTop = 0, indentLeft = 0;
+
+void trackbarIndTop(int pos, void*)
+{
+	edgeBlur(img, filterImg, indentTop, indentLeft);
+	imshow("Edge blur", filterImg);
+}
+
+void trackbarIndLeft(int pos, void*)
+{
+	edgeBlur(img, filterImg, indentTop, indentLeft);
+	imshow("Edge blur", filterImg);
+}
+
+int main(int argc, char* argv[])
+{
+	char* filename;
+
+    if (argc == 2)
+    {
+        filename = argv[1];
+    }
+    else
+    {
+        printf("Couldn't open image\n");
+        return 0;
+    }
+    img = imread(filename);
+
+    namedWindow("Edge blur");
+    createTrackbar("Indent top", "Edge blur", &indentTop, (int)(img.rows / 2.0f) - 20, trackbarIndTop);
+    createTrackbar("Indent left", "Edge blur", &indentLeft, (int)(img.cols / 2.0f) - 20, trackbarIndLeft);
+
+    namedWindow("Image");
+
+    imshow("Image", img);
+    imshow("Edge blur", img);
+    while(1)
+    {
+    waitKey();}
+    return 0;
+}
