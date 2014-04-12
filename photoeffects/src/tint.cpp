0e23cbdd755b005ffd23296151c3ea528a5d1255
KruchDmitriy <kruch.dmitriy@gmail.com>
2014-04-12 11:48:25 +0400

    Correct remarks

--- photoeffects/src/tint.cpp
+++ photoeffects/src/tint.cpp
@@ -2,7 +2,22 @@
 
 using namespace cv;
 
-int tint(InputArray src, OutputArray dst, Vec3b* colorTint, float density)
+int tint(InputArray src, OutputArray dst, const Vec3b &colorTint, float density)
 {
+    CV_Assert(src.type() == CV_8UC3);
+    Mat image = src.getMat(), outputImage(image.size(), CV_8UC3);
+    for (int i = 0; i < image.rows; i++)
+    {
+        for (int j = 0; j < image.cols; j++)
+        {
+            Vec3b colorDest = image.at<Vec3b>(i, j);
+            colorDest[0] = (uchar)((colorTint[0] - colorDest[0])*density + colorDest[0]);
+            colorDest[1] = (uchar)((colorTint[1] - colorDest[1])*density + colorDest[1]);
+            colorDest[2] = (uchar)((colorTint[2] - colorDest[2])*density + colorDest[2]);
+            outputImage.at<Vec3b>(i, j) = colorDest;
+        }
+    }
+    outputImage.convertTo(dst, CV_8UC3);
+
     return 0;
 }
