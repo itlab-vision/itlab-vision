2b4af40b641a288a91aaba971b57fb710478ac48
KruchDmitriy <kruch.dmitriy@gmail.com>
2014-04-12 12:10:06 +0400

    Delete new from files tint and edgeBlur

--- photoeffects/samples/tint_sample.cpp
+++ photoeffects/samples/tint_sample.cpp
@@ -3,73 +3,7 @@
 
 using namespace cv;
 
-Vec3b ColorTint;
-Mat BaseColor, img, filterImg;
-int valueHue = 0;
-int valueDen = 50;
-
-void trackbarTint(int pos, void*)
-{
-    Mat Color;
-    BaseColor.copyTo(Color);
-    Rect r(pos - 1, 0, 4, 20);
-    rectangle(Color, r, Scalar(0));
-    imshow("Tint", Color);
-    ColorTint = BaseColor.at<Vec3b>(0, pos);
-
-    float den = (float)valueDen / 100.0;
-    tint(img, filterImg, &ColorTint, den);
-    imshow("Filter", filterImg);
-}
-
-void trackbarDen(int pos, void *)
-{
-    float den = (float)valueDen / 100.0;
-    tint(img, filterImg, &ColorTint, den);
-    imshow("Filter", filterImg);
-}
-
 int main(int argc, char* argv[])
 {
-    char* filename;
-
-    if (argc >= 2)
-    {
-        //You should run this program with parameter - path to image
-        filename = argv[1];
-    }
-    else
-    {
-        printf("Couldn't open image\n");
-        return 1;
-    }
-    img = imread(filename);
-
-    BaseColor.create(20, 360, CV_8UC3);
-    Vec3b hsv;
-    for (int j = 0; j < 360; j++)
-    {
-        hsv[0] = (j + 1) / 2;
-        hsv[1] = 255;
-        hsv[2] = 255;
-        for (int i = 0; i < 20; i++)
-        {
-            BaseColor.at<Vec3b>(i,j) = hsv;
-        }
-    }
-    cvtColor(BaseColor, BaseColor, CV_HSV2BGR);
-
-    namedWindow("Tint");
-    createTrackbar("Hue", "Tint", &valueHue, 360, trackbarTint);
-    createTrackbar("Density(%)", "Tint", &valueDen, 100, trackbarDen);
-
-    namedWindow("Image");
-    namedWindow("Filter");
-
-    imshow("Tint", BaseColor);
-    imshow("Image", img);
-    imshow("Filter", img);
-
-    waitKey();
     return 0;
 }
--- photoeffects/src/edgeBlur.cpp
+++ photoeffects/src/edgeBlur.cpp
@@ -3,75 +3,7 @@
 
 using namespace cv;
 
-#define MAX_KERNELSIZE 15
-
 int edgeBlur(InputArray src, OutputArray dst, int indentTop, int indentLeft)
 {
-    Mat image = src.getMat(), outputImage(image.size(), CV_8UC3);
-
-    Mat bearingImage(image.rows + 2 * MAX_KERNELSIZE,
-                    image.cols + 2 * MAX_KERNELSIZE,
-                    CV_8UC3);
-    copyMakeBorder(image, bearingImage, MAX_KERNELSIZE, MAX_KERNELSIZE,
-        MAX_KERNELSIZE, MAX_KERNELSIZE, BORDER_REPLICATE);
-
-    float radius;
-    int size;
-    int sumB, sumG, sumR;
-    Vec3b Color;
-    for (int i = MAX_KERNELSIZE; i < (bearingImage.rows - MAX_KERNELSIZE); i++)
-    {
-        for (int j = MAX_KERNELSIZE; j < (bearingImage.cols - MAX_KERNELSIZE); j++)
-        {
-            radius = (bearingImage.rows / 2.0f - i)
-                    * (bearingImage.rows / 2.0f - i)
-                    / (image.rows / 2.0f - indentTop)
-                    / (image.rows / 2.0f - indentTop)
-
-                    + (bearingImage.cols / 2.0f - j)
-                    * (bearingImage.cols / 2.0f - j)
-                    / (image.cols / 2.0f - indentLeft)
-                    / (image.cols / 2.0f - indentLeft);
-            if (radius < 1.0f)
-            {
-                outputImage.at<Vec3b>(i - MAX_KERNELSIZE, j - MAX_KERNELSIZE) =
-                    bearingImage.at<Vec3b>(i, j);
-                continue;
-            }
-            else
-            {
-                sumB = sumG = sumR = 0;
-                size = (int)(radius + 0.5f) /*- rand() % 2*/;
-                if (size == 0)
-                {
-                    outputImage.at<Vec3b>(i - MAX_KERNELSIZE, j - MAX_KERNELSIZE) =
-                        bearingImage.at<Vec3b>(i, j);
-                    continue;
-                }
-                else if (size > MAX_KERNELSIZE)
-                {
-                    size = MAX_KERNELSIZE;
-                }
-                for (int x = i - size; x <= i + size; x++)
-                {
-                    for (int y = j - size; y <= j + size; y++)
-                    {
-                        Color = bearingImage.at<Vec3b>(x, y);
-                        sumB = sumB + Color[0];
-                        sumG = sumG + Color[1];
-                        sumR = sumR + Color[2];
-                    }
-                }
-                sumB = (int)(sumB / ((2.0f * size + 1.0f) * (2.0f * size + 1.0f)) + 0.5f);
-                sumG = (int)(sumG / ((2.0f * size + 1.0f) * (2.0f * size + 1.0f)) + 0.5f);
-                sumR = (int)(sumR / ((2.0f * size + 1.0f) * (2.0f * size + 1.0f)) + 0.5f);
-                Color = Vec3b(sumB, sumG, sumR);
-                outputImage.at<Vec3b>(i - MAX_KERNELSIZE, 
-                                    j - MAX_KERNELSIZE) = Color;
-            }
-        }
-    }
-
-    outputImage.convertTo(dst, CV_8UC3);
     return 0;
 }
