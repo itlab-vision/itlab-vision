b6415c9a6abe285a6ce47ac6346e65c985b6e549
Dolotov Evgeniy <dolotov.evgeniy@gmail.com>
2014-04-13 21:47:48 +0400

    add test

--- photoeffects/test/filmGrain_test.cpp
+++ photoeffects/test/filmGrain_test.cpp
@@ -1,11 +1,20 @@
-#include <gtest/gtest.h>
-
 #include "photoeffects.hpp"
+#include "test_utils.hpp"
+#include <gtest/gtest.h>
 
 using namespace cv;
 
-TEST(photoeffects, FilmGrainTest) {
-    Mat image(10, 10, CV_8UC3);
+TEST(photoeffects, FilmGrainInvalidImageFormat)
+{
+    Mat src(10, 20, CV_8UC2);
+    Mat dst;
 
-    EXPECT_EQ(0, filmGrain(image,image,5));
+    EXPECT_ERROR(CV_StsAssert, filmGrain(src,dst,5));
+}
+TEST(photoeffects, FilmGrainTest) {
+    Mat imageWithOneChannel(10, 20, CV_8UC1);
+    Mat imageWithThreeChannel(10,20,CV_8UC3);
+    Mat dst;
+    EXPECT_EQ(0, filmGrain(imageWithOneChannel,dst,5));
+    EXPECT_EQ(0, filmGrain(imageWithThreeChannel,dst,5));
 }
