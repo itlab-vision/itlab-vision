10f56f8731c927ae07c5c15d6d99e238f375a760
Vlad <vlad@vlad-Q330.(none)>
2014-04-13 15:39:13 +0400

    changed google tests

--- photoeffects/test/vignette_test.cpp
+++ photoeffects/test/vignette_test.cpp
@@ -5,6 +5,9 @@ using namespace cv;
 
 TEST(photoeffects, VignetteTest)
 {
-    //Mat image(10, 10, CV_8UC3);
-    //EXPECT_EQ(10, vignette(image, img_dst, 0, 0, 100));
+    Mat image(100, 100, CV_8UC3), dst;
+    Size rectangle;
+    rectangle.width = image.cols / 2;
+    rectangle.height = image.rows / 2;
+    EXPECT_EQ(0, vignette(image, dst, rectangle));
 }
