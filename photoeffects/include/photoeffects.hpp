cc863edc3ee686d075d5119712d02a556bf89819
KruchDmitriy <kruch.dmitriy@gmail.com>
2014-04-13 19:40:58 +0400

    Add an assert

--- photoeffects/include/photoeffects.hpp
+++ photoeffects/include/photoeffects.hpp
@@ -14,6 +14,8 @@ cv::Mat tint(cv::Mat& image, int hue, float sat);
 
 int glow(cv::InputArray src, cv::OutputArray dst, float sigma = 1.0f, float intensity = 0.0f);
 
+int edgeBlur(cv::InputArray src, cv::OutputArray dst, int indentTop, int indentLeft);
+
 int boostColor(cv::InputArray src, cv::OutputArray dst, float intensity = 0.0f);
 
 cv::Mat antique(cv::Mat& image);
