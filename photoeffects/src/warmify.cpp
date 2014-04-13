4cf1cb08ea72a0f88a2306aa8c5fdb9b879ce23c
Vlad <vlad@vlad-Q330.(none)>
2014-04-13 16:32:48 +0400

    applyed format code of warmify.cpp and used CV_Assert

--- photoeffects/src/warmify.cpp
+++ photoeffects/src/warmify.cpp
@@ -2,35 +2,48 @@
 
 using namespace cv;
 
+/*
+// Function for applying warmify filter to image
+//
+// API
+// int warmify(cv::InputArray src, cv::OutputArray dst, uchar delta);
+// INPUT
+// src- source image
+// delta - value, increase a warm components of colors which on
+// OUTPUT
+// dst - the image with warmify filter effect
+// RESULT
+// Error status
+*/
+
 int warmify(cv::InputArray src, cv::OutputArray dst, uchar delta)
 {
-    if (src.type() != CV_8UC3)
-    {
-        return 1;
-    }
-    Mat img_src(src.size(), CV_8UC3);
-    img_src = src.getMat();
-    Mat img_dst(img_src.size(), CV_8UC3);
-    Vec3b intensity, intensity_new;
+    CV_Assert(src.type() == CV_8UC3);
+
+    Mat imgSrc(src.size(), CV_8UC3);
+    imgSrc = src.getMat();
+    Mat imgDst(imgSrc.size(), CV_8UC3);
+
+    Vec3b intensity, intensityNew;
     int r = 0;
     int g = 0;
 
-    for (int i = 0; i < img_src.rows; i++)
+    for (int i = 0; i < imgSrc.rows; i++)
     {
-        for (int j = 0; j < img_src.cols; j++)
+        for (int j = 0; j < imgSrc.cols; j++)
         {
-            intensity = img_src.at<Vec3b>(i, j);
-            intensity_new.val[0] = intensity.val[0];
+            intensity = imgSrc.at<Vec3b>(i, j);
+            intensityNew.val[0] = intensity.val[0];
             g = intensity.val[1] + delta;
             if (g > 255) g = 255;
             r = intensity.val[2] + delta;
             if (r > 255) r = 255;
-            intensity_new.val[1] = (uchar)g;
-            intensity_new.val[2] = (uchar)r;
-            img_dst.at<Vec3b>(i, j) = intensity_new;
+            intensityNew.val[1] = (uchar)g;
+            intensityNew.val[2] = (uchar)r;
+            imgDst.at<Vec3b>(i, j) = intensityNew;
         }
     }
-    img_dst.convertTo(dst, img_src.type());
+    imgDst.convertTo(dst, CV_8UC3);
     return 0;
 }
 
