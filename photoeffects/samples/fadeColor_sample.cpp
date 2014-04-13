6e9309e271a625279499b9da26e78cb2c1e8e4c7
Dolotov Evgeniy <dolotov.evgeniy@gmail.com>
2014-04-13 22:09:21 +0400

    add spaces

--- photoeffects/samples/fadeColor_sample.cpp
+++ photoeffects/samples/fadeColor_sample.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 const char *ORIGINAL_IMAGE="Original image";
 const char *FADED_IMAGE="Faded image";
-const char *helper ="./fadeColor <img>\n\t<img> - file name contained the processed image\n";
-
+const char *helper =
+"./fadeColor_sample <img>\n\
+\t<img> - file name contained the processed image\n\
+";
 Point startPoint,endPoint;
 int numberChoosenPoint=0;
 int codeError;
@@ -22,9 +24,9 @@ int main(int argc, char** argv)
         cout << helper << endl;
         return 1;
     }
-    namedWindow(ORIGINAL_IMAGE,CV_WINDOW_AUTOSIZE);
-    imshow(ORIGINAL_IMAGE,src);
-    setMouseCallback(ORIGINAL_IMAGE, CallBackFunc,&src);
+    namedWindow(ORIGINAL_IMAGE, CV_WINDOW_AUTOSIZE);
+    imshow(ORIGINAL_IMAGE, src);
+    setMouseCallback(ORIGINAL_IMAGE, CallBackFunc, &src);
     cout << "Choose two points on image and press any key."<<endl;
     waitKey(0);
     destroyAllWindows();
@@ -36,7 +38,7 @@ int processArguments(int argc, char **argv, Mat &img)
     {
         return 1;
     }
-    img = imread(argv[1],CV_LOAD_IMAGE_COLOR);
+    img = imread(argv[1], CV_LOAD_IMAGE_COLOR);
     return 0;
 }
 void CallBackFunc(int event, int x, int y, int flags, void* userdata)
@@ -49,21 +51,21 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata)
         switch(numberChoosenPoint)
         {
         case 0:
-             numberChoosenPoint++;
-            startPoint=Point(x,y);
-            circle(srcCopy,Point(x,y),5,CV_RGB(255,50,255),4);
-            imshow(ORIGINAL_IMAGE,srcCopy);
+            numberChoosenPoint++;
+            startPoint=Point(x, y);
+            circle(srcCopy, Point(x,y), 5, CV_RGB(255,50,255) ,4);
+            imshow(ORIGINAL_IMAGE, srcCopy);
             break;
         case 1:
             numberChoosenPoint++;
-            endPoint=Point(x,y);
-            circle(srcCopy,startPoint,5,CV_RGB(255,50,255),4);
-            circle(srcCopy,endPoint,5,CV_RGB(255,50,255),4);
+            endPoint=Point(x, y);
+            circle(srcCopy, startPoint, 5, CV_RGB(255,50,255), 4);
+            circle(srcCopy, endPoint, 5, CV_RGB(255,50,255), 4);
             Mat dst;
-            fadeColor(src,dst,startPoint,endPoint);
-            imshow(ORIGINAL_IMAGE,srcCopy);
-            namedWindow(FADED_IMAGE,CV_WINDOW_AUTOSIZE);
-            imshow(FADED_IMAGE,dst);
+            fadeColor(src, dst, startPoint,  endPoint);
+            imshow(ORIGINAL_IMAGE, srcCopy);
+            namedWindow(FADED_IMAGE, CV_WINDOW_AUTOSIZE);
+            imshow(FADED_IMAGE, dst);
             break;
         }
     }
--- photoeffects/test/fadeColor_test.cpp
+++ photoeffects/test/fadeColor_test.cpp
@@ -11,37 +11,37 @@ TEST(photoeffects, FadeColorInvalidImageFormat)
     Mat src(10, 20, CV_8UC2);
     Mat dst;
 
-    EXPECT_ERROR(CV_StsAssert, fadeColor(src,dst,Point(5,5),Point(5,10)));
+    EXPECT_ERROR(CV_StsAssert, fadeColor(src, dst, Point(5, 5), Point(5, 10)));
 }
 TEST(photoeffects, FadeColorInvalidArgument)
 {
     Mat src(10, 20, CV_8UC1);
     Mat dst;
 
-    EXPECT_ERROR(CV_StsAssert, fadeColor(src,dst,Point(50,5),Point(5,10)));
-    EXPECT_ERROR(CV_StsAssert, fadeColor(src,dst,Point(5,5),Point(5,-10)));
-    EXPECT_ERROR(CV_StsAssert, fadeColor(src,dst,Point(5,5),Point(5,5)));
+    EXPECT_ERROR(CV_StsAssert, fadeColor(src, dst, Point(50,5), Point(5,10)));
+    EXPECT_ERROR(CV_StsAssert, fadeColor(src, dst, Point(5,5), Point(5,-10)));
+    EXPECT_ERROR(CV_StsAssert, fadeColor(src, dst, Point(5,5), Point(5,5)));
 }
 TEST(photoeffects, FadeColorTest) {
     Mat imageWithOneChannel(10, 20, CV_8UC1);
-    Mat imageWithThreeChannel(10,20,CV_8UC3);
+    Mat imageWithThreeChannel(10, 20, CV_8UC3);
     Mat dst;
-    EXPECT_EQ(0, fadeColor(imageWithOneChannel,dst,Point(5,5),Point(5,10)));
-    EXPECT_EQ(0, fadeColor(imageWithThreeChannel,dst,Point(5,5),Point(5,10)));
+    EXPECT_EQ(0, fadeColor(imageWithOneChannel, dst, Point(5,5), Point(5,10)));
+    EXPECT_EQ(0, fadeColor(imageWithThreeChannel, dst, Point(5,5), Point(5,10)));
 }
 TEST(photoeffects, FadeColorRegressionTest)
 {
     Mat image, dst, rightDst;
-    image = imread(ORIGINAL_IMAGE_TEST,  CV_LOAD_IMAGE_COLOR);
-    rightDst = imread(FADE_IMAGE_TEST,  CV_LOAD_IMAGE_COLOR);
+    image = imread(ORIGINAL_IMAGE_TEST, CV_LOAD_IMAGE_COLOR);
+    rightDst = imread(FADE_IMAGE_TEST, CV_LOAD_IMAGE_COLOR);
 
-    EXPECT_EQ(0, fadeColor(image, dst, Point(100,100),Point(250,250)));
+    EXPECT_EQ(0, fadeColor(image, dst, Point(100, 100), Point(250, 250)));
 
-    for (int i = 0; i < dst.rows; i++)
+    for (int i=0; i<dst.rows; i++)
     {
-        for (int j = 0; j < dst.cols; j++)
+        for (int j=0; j<dst.cols; j++)
         {
-            for (int k = 0; k < 3; k++)
+            for (int k=0; k<3; k++)
             {
                 ASSERT_EQ(rightDst.at<Vec3b>(i, j)[k], dst.at<Vec3b>(i, j)[k]);
             }
