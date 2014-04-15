2dd8b5a056df930e82693c78af086e691280eb7d
KruchDmitriy <kruch.dmitriy@gmail.com>
2014-04-15 20:01:40 +0400

    Correct the mistakes

--- photoeffects/src/tint.cpp
+++ photoeffects/src/tint.cpp
@@ -7,13 +7,15 @@ int tint(InputArray src, OutputArray dst,
 {
     CV_Assert(src.type() == CV_8UC3);
     CV_Assert(density >= 0.0f || density <= 1.0f);
-    Mat image = src.getMat(), outputImage(image.size(), CV_8UC3);
+    dst.create(src.size(), CV_8UC3);
+    Mat image = src.getMat(), outputImage = dst.getMat();
+    float negDen = 1 - density;
     for (int i = 0; i < image.rows; i++)
     {
         for (int j = 0; j < image.cols; j++)
         {
             Vec3b colorDest = image.at<Vec3b>(i, j);
-            colorDest = colorTint * density + (1 - density) * colorDest;
+            colorDest = colorTint * density + negDen * colorDest;
             outputImage.at<Vec3b>(i, j) = colorDest;
         }
     }
