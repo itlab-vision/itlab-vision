84f0c0c76ecfde7af6eae1b2ccb1f7b9822787fb
KruchDmitriy <kruch.dmitriy@gmail.com>
2014-04-15 13:25:09 +0400

    Correct the mistakes

--- photoeffects/src/edgeBlur.cpp
+++ photoeffects/src/edgeBlur.cpp
@@ -8,7 +8,8 @@ using namespace cv;
 int edgeBlur(InputArray src, OutputArray dst, int indentTop, int indentLeft)
 {
     CV_Assert(src.type() == CV_8UC3);
-    Mat image = src.getMat(), outputImage(image.size(), CV_8UC3);
+    dst.create(src.size(), src.type());
+    Mat image = src.getMat(), outputImage = dst.getMat();
     
     CV_Assert(indentTop >= 0 && indentTop <= (image.rows / 2 - 10));
     CV_Assert(indentLeft >= 0 && indentLeft <= (image.cols / 2 - 10));
@@ -21,29 +22,25 @@ int edgeBlur(InputArray src, OutputArray dst, int indentTop, int indentLeft)
             * (image.rows / 2.0f - indentTop);
     int kSizeEdges = halfWidth / a + halfHeight / b;
 
-    if (kSizeEdges > MAX_KERNELSIZE)
-    {
-        kSizeEdges = MAX_KERNELSIZE;
-    }
+    kSizeEdges = MIN(kSizeEdges, MAX_KERNELSIZE);
     Mat bearingImage(image.rows + 2 * kSizeEdges,
                     image.cols + 2 * kSizeEdges,
                     CV_8UC3);
     copyMakeBorder(image, bearingImage, kSizeEdges, kSizeEdges,
         kSizeEdges, kSizeEdges, BORDER_REPLICATE);
 
-    int size, maxI = bearingImage.rows - kSizeEdges,
-    maxJ = bearingImage.cols - kSizeEdges;
-    Vec3f sumF;
-    Vec3b Color;
-    float radius, sumC, coeff;
-    float bearHalfWidth = bearingImage.cols / 2.0f;
-    float bearHalfHeight = bearingImage.rows / 2.0f;
 
-    for (int i = kSizeEdges; i < maxI; i++)
+    for (int i = kSizeEdges; i < bearingImage.rows - kSizeEdges; i++)
     {
-        for (int j = kSizeEdges; j < maxJ; j++)
+        for (int j = kSizeEdges; j < bearingImage.cols - kSizeEdges; j++)
         {
-            radius = (bearHalfHeight - i)
+            int size;
+            Vec3f sumF;
+            Vec3b Color;
+            float sumC = 0.0f, coeff;
+            float bearHalfWidth = bearingImage.cols / 2.0f;
+            float bearHalfHeight = bearingImage.rows / 2.0f;
+            float radius = (bearHalfHeight - i)
                     * (bearHalfHeight - i)
                     / b
 
@@ -56,17 +53,9 @@ int edgeBlur(InputArray src, OutputArray dst, int indentTop, int indentLeft)
                     bearingImage.at<Vec3b>(i, j);
                 continue;
             }
-            sumF[0] = 0.0f;
-            sumF[1] = 0.0f;
-            sumF[2] = 0.0f;
-            sumC = 0.0f;
             size = radius;
-            radius -= 0.5f;
-            radius *= 2.0f * radius;
-            if (size > kSizeEdges)
-            {
-                size = kSizeEdges;
-            }
+            radius = 2.0f * (radius - 0.5f) * (radius - 0.5f);
+            size = MIN(size, kSizeEdges);
             for (int x = -size; x <= size; x++)
             {
                 for (int y = -size; y <= size; y++)
@@ -75,15 +64,14 @@ int edgeBlur(InputArray src, OutputArray dst, int indentTop, int indentLeft)
                             * exp(- (x * x + y * y) / radius);
 
                     Color = bearingImage.at<Vec3b>(x + i, y + j);
-                    sumF = sumF + (Vec3f) (coeff * Color);
+                    sumF += (Vec3f) (coeff * Color);
                     sumC += coeff;
                 }
             }
-            sumF = sumF * (1.0f / sumC);
+            sumF *= (1.0f / sumC);
             outputImage.at<Vec3b>(i - kSizeEdges, j - kSizeEdges) = sumF;
         }
     }
 
-    outputImage.convertTo(dst, CV_8UC3);
     return 0;
 }
