f1fee39475951ef4ad031f88f82f952c24425184
Skrebkov Artem <artem@ubuntu.(none)>
2014-04-16 23:30:09 +0400

    fix regression test

--- photoeffects/test/glow_test.cpp
+++ photoeffects/test/glow_test.cpp
@@ -11,24 +11,25 @@ TEST(photoeffects, GlowTest) {
 }
 
 TEST(photoeffects, GlowTestRegression) {
-    Mat image, dst, rightDst;
+    string input = "./testdata/glow_test.png";
+    string expectedOutput = "./testdata/glow_test_result.png";
 
-    image = imread("../itlab-vision/photoeffects/test/images/glow_test.png", CV_LOAD_IMAGE_COLOR);
-    rightDst = imread("../itlab-vision/photoeffects/test/images/glow_test_result.png", CV_LOAD_IMAGE_COLOR);
+    Mat image, rightDst;
 
+    image = imread(input, CV_LOAD_IMAGE_COLOR);
+    rightDst = imread(expectedOutput, CV_LOAD_IMAGE_COLOR);
+
+    if (image.empty())
+        FAIL() << "Can't read " + input + " image";
+    if (rightDst.empty())
+        FAIL() << "Can't read " + expectedOutput + " image";
+
+    Mat dst;
     EXPECT_EQ(0, glow(image, dst, 7.0f, 0.9f));
 
-    for (int i = 0; i < dst.rows; i++)
-    {
-        for (int j = 0; j < dst.cols; j++)
-        {
-            for (int k = 0; k < 3; k++)
-            {
-                ASSERT_LE(rightDst.at<Vec3b>(i, j)[k] - 1, dst.at<Vec3b>(i, j)[k]);
-                ASSERT_GE(rightDst.at<Vec3b>(i, j)[k] + 1, dst.at<Vec3b>(i, j)[k]);
-            }
-        }
-    }
+    Mat diff = abs(rightDst - dst);
+    Mat mask = diff.reshape(1) > 1;
+    EXPECT_EQ(0, countNonZero(mask));
 }
 
 TEST(photoeffects, GlowTestBadSigma) {
