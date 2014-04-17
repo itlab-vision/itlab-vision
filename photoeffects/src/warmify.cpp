7df43b281281c4fba1b58540779397d7f6cffcc6
Vlad <vlad@vlad-Q330.(none)>
2014-04-17 15:19:47 +0400

    warmify.cpp was be corrected by according notes

--- photoeffects/src/warmify.cpp
+++ photoeffects/src/warmify.cpp
@@ -9,7 +9,6 @@ int warmify(cv::InputArray src, cv::OutputArray dst, uchar delta)
     CV_Assert(imgSrc.data);
     Mat imgDst(imgSrc.size(), CV_8UC3);
     Vec3b intensity, intensityNew;
-    int r = 0, g = 0;
 
     for (int i = 0; i < imgSrc.rows; i++)
     {
@@ -17,10 +16,8 @@ int warmify(cv::InputArray src, cv::OutputArray dst, uchar delta)
         {
             intensity = imgSrc.at<Vec3b>(i, j);
             intensityNew[0] = intensity[0];
-            g = intensity[1] + delta;
-            intensityNew[1] = (g < 255) ? (uchar)g : 255;
-            r = intensity[2] + delta;
-            intensityNew[2] = (r < 255) ? (uchar)r : 255;
+            intensityNew[1] = (intensity[1] + delta < 255) ? (uchar)(intensity[1] + delta) : 255;
+            intensityNew[2] = (intensity[2] + delta < 255) ? (uchar)(intensity[2] + delta) : 255;
             imgDst.at<Vec3b>(i, j) = intensityNew;
         }
     }
