9ba2d0d13e59c8cc2cd97891a50f143093bb3eda
Дмитрий <kruch.dmitriy@gmail.com>
2014-04-19 10:08:50 +0400

    Update tint_test.cpp

--- photoeffects/test/tint_test.cpp
+++ photoeffects/test/tint_test.cpp
@@ -28,22 +28,22 @@ TEST(photoeffects, TintWrongDensity)
 
 TEST(photoeffects, TintTestOutputImage)
 {
-	Mat src(256, 256, CV_8UC3), dstOrigin(256, 256, CV_8UC3), dstFromAlg;
-	Vec3b color(255, 0, 255);
-	float den = 0.2f;
-	float negDen = 1 - den;
+    Mat src(256, 256, CV_8UC3), dstOrigin(256, 256, CV_8UC3), dstFromAlg;
+    Vec3b color(255, 0, 255);
+    float den = 0.2f;
+    float negDen = 1 - den;
 
-	for (int i = 0; i < 256; i++)
-	{
-		for (int j = 0; j < 256; j++)
-		{
-			src.at<Vec3b>(i, j) = Vec3b(i, j, 0);
-			dstOrigin.at<Vec3b>(i, j) = color * den + Vec3b(i, j, 0) * negDen;
-		}
-	}
-	tint(src, dstFromAlg, color, den);
+    for (int i = 0; i < 256; i++)
+    {
+        for (int j = 0; j < 256; j++)
+        {
+             src.at<Vec3b>(i, j) = Vec3b(i, j, 0);
+             dstOrigin.at<Vec3b>(i, j) = color * den + Vec3b(i, j, 0) * negDen;
+        }
+    }
+    tint(src, dstFromAlg, color, den);
 
-	Mat diff = abs(dstOrigin - dstFromAlg);
+    Mat diff = abs(dstOrigin - dstFromAlg);
     Mat mask = diff.reshape(1) > 1;
     EXPECT_EQ(0, countNonZero(mask));
 }
