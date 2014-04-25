554db595ae5f460cdbcee6f2a6a083c61d3fb44e
KruchDmitriy <kruch.dmitriy@gmail.com>
2014-04-25 13:53:21 +0400

    Add a regression test

--- photoeffects/test/tint_test.cpp
+++ photoeffects/test/tint_test.cpp
@@ -25,3 +25,26 @@ TEST(photoeffects, TintWrongDensity)
     EXPECT_ERROR(CV_StsAssert, tint(src, dst, color, 15.0f));
     EXPECT_ERROR(CV_StsAssert, tint(src, dst, color, -1.0f));
 }
+
+TEST(photoeffects, TintRegressionTest)
+{
+    string input = "./testdata/tint_test.png";
+    string expectedOutput = "./testdata/tint_test_result.png";
+
+    Mat src, dst, rightDst;
+
+    src = imread(input, CV_LOAD_IMAGE_COLOR);
+    rightDst = imread(expectedOutput, CV_LOAD_IMAGE_COLOR);
+
+    if (src.empty())
+        FAIL() << "Can't read " + input + " image";
+    if (rightDst.empty())
+        FAIL() << "Can't read " + expectedOutput + " image";
+
+    Vec3b color(128, 255, 0);
+    EXPECT_EQ(0, tint(src, dst, color, 0.1f));
+
+    Mat diff = abs(rightDst - dst);
+    Mat mask = diff.reshape(1) > 1;
+    EXPECT_EQ(0, countNonZero(mask));
+}
