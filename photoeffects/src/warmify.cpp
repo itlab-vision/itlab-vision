524e507e2dd26f32e8abdc70ea423d74e168fddc
Vlad <vlad@vlad-Q330.(none)>
2014-05-05 22:41:34 +0400

    warmify.cpp changed by using OpenCV methods

--- photoeffects/src/warmify.cpp
+++ photoeffects/src/warmify.cpp
@@ -9,21 +9,9 @@ int warmify(cv::InputArray src, cv::OutputArray dst, uchar delta)
     CV_Assert(imgSrc.data);
     dst.create(src.size(), CV_8UC3);
     Mat imgDst = dst.getMat();
-    Vec3b intensity, intensityNew;
 
-    for (int i = 0; i < imgSrc.rows; i++)
-    {
-        for (int j = 0; j < imgSrc.cols; j++)
-        {
-            intensity = imgSrc.at<Vec3b>(i, j);
-            intensityNew[0] = intensity[0];
-            uchar g = (intensity[1] + delta) >> 8;
-            uchar r = (intensity[2] + delta) >> 8;
-            intensityNew[1] = g * 255 + (1 - g) * intensity[1] + (1 - g) * delta;
-            intensityNew[2] = r * 255 + (1 - r) * intensity[2] + (1 - r) * delta;
-            imgDst.at<Vec3b>(i, j) = intensityNew;
-        }
-    }
+    Mat warmifyMat(src.size(), CV_8UC3, Scalar(0, delta, delta));
+
+    imgDst = imgSrc + warmifyMat;
     return 0;
 }
-
