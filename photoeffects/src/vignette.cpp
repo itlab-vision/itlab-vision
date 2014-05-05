570d4512ddcf7ce4d6ce6345d419f1b5aaaa4033
Vlad <vlad@vlad-Q330.(none)>
2014-05-05 21:34:06 +0400

    sinFi deleted and code corrected

--- photoeffects/src/vignette.cpp
+++ photoeffects/src/vignette.cpp
@@ -1,9 +1,20 @@
 #include "photoeffects.hpp"
 
+#if 1
+    #include <stdio.h>
+    #define TIMER_START(name) int64 t_##name = getTickCount()
+    #define TIMER_END(name) printf("TIMER_" #name ":\t%6.2fms\n", \
+                1000.f * ((getTickCount() - t_##name) / getTickFrequency()))
+#else
+    #define TIMER_START(name)
+    #define TIMER_END(name)
+#endif
+
 using namespace cv;
 
 int vignette(InputArray src, OutputArray dst, Size rect)
 {
+    TIMER_START(Initialize);
     CV_Assert(src.type() == CV_8UC3 && rect.height != 0 && rect.width != 0);
 
     Mat imgSrc = src.getMat();
@@ -11,39 +22,31 @@ int vignette(InputArray src, OutputArray dst, Size rect)
 
     dst.create(imgSrc.size(), CV_8UC3);
     Mat imgDst = dst.getMat();
-    Vec3b intensity, intensityNew;
 
     float centerRow = imgSrc.rows / 2.0f;
     float centerCol = imgSrc.cols / 2.0f;
     float aSquare = rect.height * rect.height / 4.0f;
     float bSquare = rect.width * rect.width / 4.0f;
-    float ab = rect.height * rect.width / 4.0f;
-    float radiusMax = sqrtf(centerRow * centerRow + centerCol * centerCol);
+    float radiusMax = centerRow * centerRow / aSquare + centerCol * centerCol / bSquare - 1;
+    TIMER_END(Initialize);
 
+    TIMER_START(Main);
     for (int i = 0; i < imgSrc.rows; i++)
     {
-        float iMinusCenterRow = (i - centerRow);
-        float iMinusCenterRowSquare = iMinusCenterRow * iMinusCenterRow;
         for (int j = 0; j < imgSrc.cols; j++)
         {
-            intensity = imgSrc.at<Vec3b>(i, j);
+            Vec3b intensity = imgSrc.at<Vec3b>(i, j);
 
-            float jMinusCenterColSquare = (j - centerCol) * (j - centerCol);
-            intensityNew = intensity;
-            if (iMinusCenterRowSquare / aSquare + jMinusCenterColSquare / bSquare > 1.0f)
+            float dist = (i - centerRow) * (i - centerRow) / aSquare +
+                    (j - centerCol) * (j - centerCol) / bSquare;
+            if (dist > 1.0f)
             {
-                float dist = sqrtf(iMinusCenterRowSquare + jMinusCenterColSquare);
-                float sinFi = iMinusCenterRow / dist;
-                float sinFiSquare = sinFi * sinFi;
-                float radiusEllipse = ab / sqrtf(aSquare * (1.0f - sinFiSquare) +
-                                          bSquare * sinFiSquare);
-                float coefficient = 1.0f - ((dist - radiusEllipse) /
-                                      (radiusMax - radiusEllipse));
-
-                intensityNew *= coefficient;
+                float coefficient = 1.0f - (dist - 1.0f) / radiusMax;
+                intensity *= coefficient;
             }
-            imgDst.at<Vec3b>(i, j) = intensityNew;
+            imgDst.at<Vec3b>(i, j) = intensity;
         }
     }
+    TIMER_END(Main);
     return 0;
 }
