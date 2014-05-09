a218440ddf35d65fa8a5dba15ef12b26fb185721
unknown <skrebkov.a@ws-2k-114-06n.ccam.cmc.unn.net>
2014-05-10 01:00:55 +0400

    change image type from cv_32f to cv_8u

--- photoeffects/src/glow.cpp
+++ photoeffects/src/glow.cpp
@@ -17,6 +17,38 @@ namespace
         Mat foreImg = foreground.getMat();
         Mat backImg = background.getMat();
 
+		result.create(backImg.size(), backImg.type());
+        Mat resultImg = result.getMat();
+
+		Mat resultImg2(backImg.size(), CV_32SC1); 
+
+		backImg.convertTo(backImg, CV_32SC3);
+		backImg = backImg.reshape(1);
+		foreImg.convertTo(foreImg, CV_32SC3);	
+		foreImg = foreImg.reshape(1);
+		
+		Mat tmp;
+		TIMER_START(slow);
+		cv::multiply(foreImg, backImg, tmp, 2); 
+		tmp /= 255;
+		Mat mask = backImg > 127;
+		cv::add(-tmp, 2 * (foreImg + backImg) - 255, resultImg2, mask);
+		cv::add(tmp, 0, resultImg2, ~mask);
+		TIMER_END(slow);
+		resultImg2 = resultImg2.reshape(3);
+		backImg = backImg.reshape(3);
+		backImg.convertTo(backImg, CV_8UC3);
+		resultImg2.convertTo(resultImg2, CV_8UC3);
+
+		resultImg2.copyTo(resultImg);
+    }
+
+	
+    void overlay3(InputArray foreground, InputArray background, OutputArray result)
+    {
+        Mat foreImg = foreground.getMat();
+        Mat backImg = background.getMat();
+
         result.create(backImg.size(), backImg.type());
         Mat resultImg = result.getMat();
 
@@ -28,21 +60,19 @@ namespace
             {
                 for(int k = 0; k < COUNT_CHANNEL; k++)
                 {
-                    float intensFore = foreImg.at<Vec3f>(i, j)[k] / 255.0f;
-                    float intensBack = backImg.at<Vec3f>(i, j)[k] / 255.0f;
-                    float intensResult = 2.0f * intensFore * intensBack;
+                    uchar intensFore = foreImg.at<Vec3b>(i, j)[k];
+                    uchar intensBack = backImg.at<Vec3b>(i, j)[k];
+					int intensResult = (2 * intensFore * intensBack) / 255;
                     
-                    if (intensBack > 0.5f)
+                    if (intensBack > 127)
                     {
-                        intensResult = -intensResult - 1.0f + 2.0f * (intensFore + intensBack);
+                        intensResult = cv::min(-intensResult - 255 +2 * (intensFore + intensBack), 255);
                     }
-    
-                    resultImg.at<Vec3f>(i,j)[k] = intensResult * 255.0f;
+					resultImg.at<Vec3b>(i, j)[k] = intensResult;
                 }
             }
         }
     }
-
 
     void overlay(InputArray foreground, InputArray background, OutputArray result)
     {
@@ -111,32 +141,33 @@ int glow(InputArray src, OutputArray dst, int radius, float intensity)
     CV_Assert(intensity >= 0.0f && intensity <= 1.0f);
 
     int srcImgType = srcImg.type();
-    if (srcImgType != CV_32FC3)
+    if (srcImgType != CV_8UC3)
     {
-		TIMER_START(to32f);
-        srcImg.convertTo(srcImg, CV_32FC3);
-		TIMER_END(to32f);
+        srcImg.convertTo(srcImg, CV_8UC3);
     }
 
     Mat blurImg;
     Size size(radius, radius);
-	TIMER_START(blur);
     GaussianBlur(srcImg, blurImg, size, 0.0f, 0.0f);
-	TIMER_END(blur);
     Mat overlayImg;
-	TIMER_START(overl);
-    overlay(blurImg, srcImg, overlayImg);
-	TIMER_END(overl);
-	overlayImg.convertTo(overlayImg, CV_8UC3);
+    overlay3(blurImg, srcImg, overlayImg);
+	/*
+	blurImg.convertTo(blurImg, CV_32FC3);
+	srcImg.convertTo(srcImg, CV_32FC3);
+	Mat overlayImg2;
+    overlay(blurImg, srcImg, overlayImg2);
+	blurImg.convertTo(blurImg, CV_8UC3);
 	srcImg.convertTo(srcImg, CV_8UC3);
-	TIMER_START(opac);
+	overlayImg2.convertTo(overlayImg2, CV_8UC3);
+	Mat diff = abs(overlayImg2 - overlayImg);
+	diff = diff.reshape(1);
+	Mat mask = diff > 1;
+	printf("%d\n", cv::countNonZero(mask));
+	*/
+
 	uchar coeff = static_cast<uchar>(intensity * 256.0 + 0.5);
-	Mat dstImg = (coeff * overlayImg - (256 - coeff) * srcImg) / 256; 
+	Mat dstImg = (coeff * overlayImg + (256 - coeff) * srcImg) / 256; 
     //opacity(overlayImg, srcImg, dst, intensity);
-	TIMER_END(opac);
-    //Mat dstImg = dst.getMat();
-    TIMER_START(toSrcType);
     dstImg.convertTo(dst, srcImgType);
-    TIMER_END(toSrcType);
     return 0;
 }
