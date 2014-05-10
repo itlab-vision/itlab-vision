a23ea1196d998fb6bfcb3eef8b095b1e10ac44ce
Vadim Levin <vadimlevin1@gmail.com>
2014-05-10 19:50:00 +0400

    trying to normalize function.

--- photoeffects/src/matte.cpp
+++ photoeffects/src/matte.cpp
@@ -36,7 +36,6 @@ int matte(InputArray src, OutputArray dst, Point firstPoint, Point secondPoint)
     CV_Assert(src.type() == CV_8UC3);
     Mat srcImg = src.getMat();
     CV_Assert(!(srcImg.empty()));
-    Mat dstImg = src.getMat();
     float xsize = firstPoint.x - secondPoint.x;
     float ysize = firstPoint.y - secondPoint.y;
     Point topLeft = topleftFind(firstPoint, secondPoint, xsize, ysize);
@@ -44,27 +43,34 @@ int matte(InputArray src, OutputArray dst, Point firstPoint, Point secondPoint)
     float bSquare = ysize*ysize/4.0f;
     float xellipseCenter = topLeft.x + xsize/2.0f;
     float yellipseCenter = topLeft.y - ysize/2.0f;
-    float aSQ = srcImg.cols*srcImg.cols*2;
-    float bSQ = srcImg.rows*srcImg.rows*2;
+    float aSQ = srcImg.cols*srcImg.cols/4.0f;
+    float bSQ = srcImg.rows*srcImg.rows/4.0f;
     float xSQ = srcImg.cols*srcImg.cols/4.0f;
     float ySQ = srcImg.rows*srcImg.rows/4.0f;
-    float coef = 0.0f;
+    float coef = 0;
+    Mat dstImg = srcImg;
+    const Vec3b whiteIntensity(255, 255, 255);
     Vec3b currentIntensity;
-    for (int i = 0; i < srcImg.rows; i++)
+    for (float i = 0; i < srcImg.rows; i++)
     {
         float ijCenterX= i - yellipseCenter;
         float ijCenterXSquare = ijCenterX*ijCenterX;
-        for(int j = 0; j < srcImg.cols; j++)
+        for(float j = 0; j < srcImg.cols; j++)
         {
             currentIntensity = srcImg.at<Vec3b>(i,j);
             float ijCenterY = j - xellipseCenter;
-            float ijCenterYSquare =ijCenterY*ijCenterY;
+            float ijCenterYSquare = ijCenterY*ijCenterY;
             if((ijCenterXSquare/bSquare + ijCenterYSquare/aSquare) > 1.0f)
             {
-                //maxkolSq = sqrt(srcImg.cols*srcImg.cols/4.0f*srcImg.rows*srcImg.rows/4.0f);
-                coef = 1.0f + sqrtf((ijCenterXSquare/bSquare + ijCenterYSquare/aSquare));
-                
-                dstImg.at<Vec3b>(i,j) = coef*currentIntensity;
+                float alpha= (ijCenterXSquare/bSQ + ijCenterYSquare/aSQ)*((ijCenterXSquare/bSquare + ijCenterYSquare/aSquare));
+                if(alpha > 1.0f)
+                {
+                    alpha = 1.0f;
+                }
+                //cout<<"alpha = "<<alpha<<" i = "<<i<<" j = "<<j<<endl;
+                coef = 1.0f - alpha;
+                //cout<<"alpha = "<<alpha<<" coef = "<<coef<<" i = "<<i<<" j = "<<j<<endl;
+                dstImg.at<Vec3b>(i,j) = coef*currentIntensity + alpha*whiteIntensity;
             }
         }
     }
