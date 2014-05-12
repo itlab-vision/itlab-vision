2e5027169fb1ce10cb7be2ded7a4f1fcdce25a79
Vlad <vlad@vlad-Q330.(none)>
2014-05-12 22:09:09 +0400

    Changed parallel code

--- photoeffects/src/vignette.cpp
+++ photoeffects/src/vignette.cpp
@@ -2,16 +2,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/internal.hpp>
 
-#if 1
-    #include <stdio.h>
-    #define TIMER_START(name) int64 t_##name = getTickCount()
-    #define TIMER_END(name) printf("TIMER_" #name ":\t%6.2fms\n", \
-                1000.f * ((getTickCount() - t_##name) / getTickFrequency()))
-#else
-    #define TIMER_START(name)
-    #define TIMER_END(name)
-#endif
-
 using namespace cv;
 
 class VignetteInvoker
@@ -24,41 +14,38 @@ private:
     VignetteInvoker& operator=(const VignetteInvoker&);
 
 public:
-    VignetteInvoker(const Mat src, Mat dst, Size rect):
+    VignetteInvoker(const Mat& src, Mat& dst, Size rect):
         imgSrc(src),
         imgDst(dst)
     {
-        float centerRow = imgSrc.rows / 2.0f;
-		float centerCol = imgSrc.cols / 2.0f;
-		float aSquare = rect.height * rect.height / 4.0f;
-		float bSquare = rect.width * rect.width / 4.0f;
-		float radiusMax = centerRow * centerRow / aSquare + centerCol * centerCol / bSquare - 1.0f;
+        centerRow = imgSrc.rows / 2.0f;
+		centerCol = imgSrc.cols / 2.0f;
+		aSquare = rect.height * rect.height / 4.0f;
+		bSquare = rect.width * rect.width / 4.0f;
+		radiusMax = centerRow * centerRow / aSquare + centerCol * centerCol / bSquare - 1.0f;
     }
 
     void operator()(const BlockedRange& rows) const
     {
     	Mat srcStripe = imgSrc.rowRange(rows.begin(), rows.end());
     	Mat dstStripe = imgDst.rowRange(rows.begin(), rows.end());
+    	srcStripe.copyTo(dstStripe);
     	int stripeWidth = srcStripe.rows;
 
-        for (int i = 0; i < stripeWidth; i++)
+        for (int i = rows.begin(); i < rows.end(); i++)
         {
-        	Vec3b* srcRow = (Vec3b*)srcStripe.row(i).data;
-        	Vec3b* dstRow = (Vec3b*)dstStripe.row(i).data;
+        	Vec3b* srcRow = (Vec3b*)srcStripe.row(i - rows.begin()).data;
+        	Vec3b* dstRow = (Vec3b*)dstStripe.row(i - rows.begin()).data;
             for (int j = 0; j < imgSrc.cols; j++)
 	        {
-
 	            float dist = (i - centerRow) * (i - centerRow) / aSquare +
 	                    (j - centerCol) * (j - centerCol) / bSquare;
+	            float coefficient = 1.0f;
 	            if (dist > 1.0f)
 	            {
-	                float coefficient = 1.0f - (dist - 1.0f) / radiusMax;
-	                dstRow[j] = srcRow[j] * coefficient;
+	                coefficient = 1.0f - (dist - 1.0f) / radiusMax;
 	            }
-	            else
-	            {
-	            	dstRow[j] = srcRow[j];
-	            }
+	            dstRow[j] *= coefficient;
 	        }
         }
     }
@@ -66,7 +53,6 @@ public:
 
 int vignette(InputArray src, OutputArray dst, Size rect)
 {
-    TIMER_START(Initialize);
     CV_Assert(src.type() == CV_8UC3 && rect.height != 0 && rect.width != 0);
 
     Mat imgSrc = src.getMat();
@@ -74,10 +60,7 @@ int vignette(InputArray src, OutputArray dst, Size rect)
 
     dst.create(imgSrc.size(), CV_8UC3);
     Mat imgDst = dst.getMat();
-    TIMER_END(Initialize);
 
-    TIMER_START(Main);
     parallel_for(BlockedRange(0, imgSrc.rows), VignetteInvoker(imgSrc, imgDst, rect));
-    TIMER_END(Main);
     return 0;
 }
