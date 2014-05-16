a68cbe10b4dd2f786d51ed275991f0dc6f83e4f4
unknown <skrebkov.a@ws-2k-114-06n.ccam.cmc.unn.net>
2014-05-16 14:25:58 +0400

    fix spaces

--- photoeffects/src/boostColor.cpp
+++ photoeffects/src/boostColor.cpp
@@ -1,10 +1,15 @@
 #include "photoeffects.hpp"
 
+    #include <stdio.h>
+    #define TIMER_START(name) int64 t_##name = getTickCount()
+    #define TIMER_END(name) printf("TIMER_" #name ":\t%6.2fms\n", \
+                1000.f * ((getTickCount() - t_##name) / getTickFrequency()))
+
 using namespace cv;
 
 int boostColor(cv::InputArray src, cv::OutputArray dst, float intensity)
 {
-	const int MAX_INTENSITY = 255;
+    const int MAX_INTENSITY = 255;
 
     Mat srcImg = src.getMat();
 
@@ -17,14 +22,15 @@ int boostColor(cv::InputArray src, cv::OutputArray dst, float intensity)
     }
 
     Mat srcHls;
+	TIMER_START(toHls);
     cvtColor(srcImg, srcHls, CV_BGR2HLS);
-
-	int intensityInt = intensity * MAX_INTENSITY;
-	srcHls += Scalar(0, 0, intensityInt);
-
+	TIMER_END(toHls);
+    int intensityInt = intensity * MAX_INTENSITY;
+    srcHls += Scalar(0, 0, intensityInt);
+	TIMER_START(toBGR);
     cvtColor(srcHls, dst, CV_HLS2BGR);
-
-	dst.getMat().convertTo(dst, srcImg.type());
-
+	TIMER_END(toBGR);
+    dst.getMat().convertTo(dst, srcImg.type());
+	imwrite("result.jpg", dst.getMat());
     return 0;
 }
