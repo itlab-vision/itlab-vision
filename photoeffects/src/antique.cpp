ee515dc1a21ad54b605ae17474fad8208adf6905
Vadim Levin <vadimlevin1@gmail.com>
2014-05-18 17:43:11 +0400

    Forgot resize.

--- photoeffects/src/antique.cpp
+++ photoeffects/src/antique.cpp
@@ -11,6 +11,7 @@ int antique(InputArray src, OutputArray dst, InputArray texture, float alpha)
     Mat textureImg = texture.getMat();
     Mat srcImg = src.getMat();
     CV_Assert((textureImg.rows >= srcImg.rows) && (textureImg.cols >= srcImg.cols));
+    resize(textureImg, textureImg, srcImg.size());
     Mat m_sepiaKernel = (Mat_<float>(3,3)<<0.272f, 0.534f, 0.131f,
                                            0.349f, 0.686f, 0.168f,
                                            0.393f, 0.769f, 0.189f);
