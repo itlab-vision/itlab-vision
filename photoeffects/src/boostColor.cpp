5d5abffc9c65d41cc1bd3d82d0719fb59fd2086f
unknown <skrebkov.a@ws-2k-114-06n.ccam.cmc.unn.net>
2014-05-26 23:42:02 +0400

    change regression test

--- photoeffects/src/boostColor.cpp
+++ photoeffects/src/boostColor.cpp
@@ -1,10 +1,5 @@
 #include "photoeffects.hpp"
 
-    #include <stdio.h>
-    #define TIMER_START(name) int64 t_##name = getTickCount()
-    #define TIMER_END(name) printf("TIMER_" #name ":\t%6.2fms\n", \
-                1000.f * ((getTickCount() - t_##name) / getTickFrequency()))
-
 using namespace cv;
 
 int boostColor(cv::InputArray src, cv::OutputArray dst, float intensity)
@@ -22,15 +17,13 @@ int boostColor(cv::InputArray src, cv::OutputArray dst, float intensity)
     }
 
     Mat srcHls;
-	TIMER_START(toHls);
     cvtColor(srcImg, srcHls, CV_BGR2HLS);
-	TIMER_END(toHls);
+	
     int intensityInt = intensity * MAX_INTENSITY;
     srcHls += Scalar(0, 0, intensityInt);
-	TIMER_START(toBGR);
+	
     cvtColor(srcHls, dst, CV_HLS2BGR);
-	TIMER_END(toBGR);
+	
     dst.getMat().convertTo(dst, srcImg.type());
-	imwrite("result.jpg", dst.getMat());
     return 0;
 }
